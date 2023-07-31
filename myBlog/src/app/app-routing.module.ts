b814fe9a6f66397b26b067113eea0c322d58ad24
sytrejo <s.y.trejo26@gmail.com>
2023-07-31 18:18:25 -0400

    updates

--- myBlog/src/app/app-routing.module.ts
+++ myBlog/src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'contact' , component: ContactComponent }
+  { path: 'contact' , component: ContactComponent },
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  { path: '', pathMatch:'full', redirectTo: 'articles'},
+  { path:'**', redirectTo: 'articles' }
 ];
 
 @NgModule({
