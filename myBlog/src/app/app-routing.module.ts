b3de4cde976e91b4ed0062441a786a2c310ee97a
sytrejo <s.y.trejo26@gmail.com>
2023-07-31 18:10:19 -0400

    updates

--- myBlog/src/app/app-routing.module.ts
+++ myBlog/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: 'contact' , component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
