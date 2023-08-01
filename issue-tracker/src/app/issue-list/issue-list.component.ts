32b5ae5a4129359ecc997f296a8cbbe8617728b8
sytrejo <s.y.trejo26@gmail.com>
2023-07-31 20:25:06 -0400

    updates

---
+++ issue-tracker/src/app/issue-list/issue-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { IssuesService } from '../issues.service';
+import { Issue } from '../issue';
+
+@Component({
+  selector: 'app-issue-list',
+  templateUrl: './issue-list.component.html',
+  styleUrls: ['./issue-list.component.css']
+})
+export class IssueListComponent  implements OnInit{
+  issues: Issue[] = [];
+
+  constructor(private issueService: IssuesService){ }
+
+  ngOnInit(): void {
+    this.getIssues();
+    throw new Error('Method not implemented.');
+  }
+
+  private getIssues(){
+    this.issues = this.issueService.getPendingIssues();
+  }
+
+}
